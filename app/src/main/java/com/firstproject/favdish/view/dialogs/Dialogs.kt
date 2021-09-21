baca70ac042cb666500bdf8d07281574fc36bacf
BulgakovIvan <bulgivan@mail.ru>
2021-09-21 07:36:10 +0300

    AddUpdateFragment refactor to Dialogs

--- app/src/main/java/com/firstproject/favdish/view/dialogs/Dialogs.kt
+++ app/src/main/java/com/firstproject/favdish/view/dialogs/Dialogs.kt
@@ -1,12 +1,97 @@
 package com.firstproject.favdish.view.dialogs
 
+import android.Manifest
+import android.app.Dialog
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat.startActivity
+import com.firstproject.favdish.databinding.DialogCustomImageSelectionBinding
+import com.firstproject.favdish.view.activities.CameraXApp
+import com.firstproject.favdish.view.fragments.AddUpdateFragment
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
+
+fun customImageSelectionDialog(
+    fragment: AddUpdateFragment,
+    tvCameraClick: ActivityResultLauncher<Intent>,
+    tvGalleryClick: ActivityResultLauncher<String>
+) {
+    val dialog = Dialog(fragment.requireActivity())
+    val binding = DialogCustomImageSelectionBinding.inflate(
+        LayoutInflater.from(fragment.requireActivity()))
+    dialog.setContentView(binding.root)
+
+    binding.tvCamera.setOnClickListener {
+        Dexter.withContext(fragment.requireActivity()).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                report?.let {
+                    if (report.areAllPermissionsGranted()) {
+                        tvCameraClick.launch(
+                            Intent(fragment.requireActivity(), CameraXApp::class.java)
+                        )
+                    }
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permission: MutableList<PermissionRequest>?,
+                token: PermissionToken?
+            ) {
+                showRationalDialogForPermissions(fragment.requireActivity())
+            }
+
+        }).onSameThread().check()
+
+        dialog.dismiss()
+    }
+
+    binding.tvGallery.setOnClickListener {
+        Dexter.withContext(fragment.requireActivity())
+            .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(response: PermissionGrantedResponse) {
+                    tvGalleryClick.launch("image/*")
+                }
+
+                override fun onPermissionDenied(response: PermissionDeniedResponse) {
+                    Toast.makeText(
+                        fragment.requireActivity(),
+                        "You have denied the storage permission to select image.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: PermissionRequest,
+                    token: PermissionToken
+                ) {
+                    showRationalDialogForPermissions(fragment.requireActivity())
+                }
+            })
+            .onSameThread()
+            .check()
+
+        dialog.dismiss()
+    }
+
+    dialog.show()
+}
 
 fun showRationalDialogForPermissions(context: Context) {
     AlertDialog
--- app/src/main/java/com/firstproject/favdish/view/fragments/AddUpdateFragment.kt
+++ app/src/main/java/com/firstproject/favdish/view/fragments/AddUpdateFragment.kt
@@ -1,20 +1,16 @@
 package com.firstproject.favdish.view.fragments
 
-import android.Manifest
 import android.app.Activity
-import android.app.Dialog
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
@@ -22,19 +18,9 @@ import androidx.lifecycle.OnLifecycleEvent
 import com.bumptech.glide.Glide
 import com.firstproject.favdish.R
 import com.firstproject.favdish.databinding.AddUpdateFragmentBinding
-import com.firstproject.favdish.databinding.DialogCustomImageSelectionBinding
 import com.firstproject.favdish.utils.saveImage
-import com.firstproject.favdish.view.activities.CameraXApp
-import com.firstproject.favdish.view.dialogs.showRationalDialogForPermissions
+import com.firstproject.favdish.view.dialogs.customImageSelectionDialog
 import com.firstproject.favdish.viewmodels.AddUpdateViewModel
-import com.karumi.dexter.Dexter
-import com.karumi.dexter.MultiplePermissionsReport
-import com.karumi.dexter.PermissionToken
-import com.karumi.dexter.listener.PermissionDeniedResponse
-import com.karumi.dexter.listener.PermissionGrantedResponse
-import com.karumi.dexter.listener.PermissionRequest
-import com.karumi.dexter.listener.multi.MultiplePermissionsListener
-import com.karumi.dexter.listener.single.PermissionListener
 
 class AddUpdateFragment : Fragment(), LifecycleObserver {
 
@@ -42,9 +28,9 @@ class AddUpdateFragment : Fragment(), LifecycleObserver {
     private val binding get() = _binding!!
 
     private val addUpdateViewModel: AddUpdateViewModel by viewModels()
-    var changeActivity: ChangeActivity? = null
+    private var changeActivity: ChangeActivity? = null
 
-    val startCameraXApp = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+    private val startCameraXApp = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
         if (result.resultCode == Activity.RESULT_OK) {
             val uri = result.data?.getStringExtra(IMAGE_URI)
             addUpdateViewModel.setImageUri(Uri.parse(uri))
@@ -54,7 +40,7 @@ class AddUpdateFragment : Fragment(), LifecycleObserver {
         }
     }
 
-    val getContent = registerForActivityResult(ActivityResultContracts.GetContent()){uri ->
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()){ uri ->
         uri.let {
             addUpdateViewModel.setImageUri(it)
             saveImage(requireActivity(), it)
@@ -70,6 +56,7 @@ class AddUpdateFragment : Fragment(), LifecycleObserver {
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    @Suppress("unused")
     fun onCreatedActivity(){
         activity?.lifecycle?.removeObserver(this)
 
@@ -96,7 +83,7 @@ class AddUpdateFragment : Fragment(), LifecycleObserver {
         super.onViewCreated(view, savedInstanceState)
 
         binding.ivAddDishImage.setOnClickListener {
-            customImageSelectionDialog()
+            customImageSelectionDialog(this,startCameraXApp, getContent)
         }
 
         addUpdateViewModel.imageUri.observe(viewLifecycleOwner) {
@@ -111,70 +98,7 @@ class AddUpdateFragment : Fragment(), LifecycleObserver {
     override fun onDestroy() {
         super.onDestroy()
         changeActivity?.hideMenu(true)
-    }
-
-    private fun customImageSelectionDialog() {
-        val dialog = Dialog(requireActivity())
-        val binding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
-        dialog.setContentView(binding.root)
-
-        binding.tvCamera.setOnClickListener {
-            Dexter.withContext(requireActivity()).withPermissions(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.CAMERA
-            ).withListener(object : MultiplePermissionsListener {
-                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                    report?.let {
-                        if (report.areAllPermissionsGranted()) {
-                            startCameraXApp.launch(
-                                Intent(requireActivity(), CameraXApp::class.java)
-                            )
-                        }
-                    }
-                }
-
-                override fun onPermissionRationaleShouldBeShown(
-                    permission: MutableList<PermissionRequest>?,
-                    token: PermissionToken?
-                ) {
-                    showRationalDialogForPermissions(requireActivity())
-                }
-
-            }).onSameThread().check()
-
-            dialog.dismiss()
-        }
-
-        binding.tvGallery.setOnClickListener {
-            Dexter.withContext(requireActivity())
-                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-                .withListener(object : PermissionListener {
-                    override fun onPermissionGranted(response: PermissionGrantedResponse) {
-                        getContent.launch("image/*")
-                    }
-
-                    override fun onPermissionDenied(response: PermissionDeniedResponse) {
-                        Toast.makeText(
-                            requireActivity(),
-                            "You have denied the storage permission to select image.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-
-                    override fun onPermissionRationaleShouldBeShown(
-                        permission: PermissionRequest,
-                        token: PermissionToken
-                    ) {
-                        showRationalDialogForPermissions(requireActivity())
-                    }
-                })
-                .onSameThread()
-                .check()
-
-            dialog.dismiss()
-        }
-
-        dialog.show()
+        _binding = null
     }
 
     interface ChangeActivity {
@@ -182,6 +106,7 @@ class AddUpdateFragment : Fragment(), LifecycleObserver {
     }
 
     companion object {
+        @Suppress("unused")
         fun newInstance() = AddUpdateFragment()
         const val IMAGE_URI = "IMAGE_URI"
     }
