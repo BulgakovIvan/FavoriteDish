94d986c29fda9db4c505ccdbc5d0f6d600f62185
BulgakovIvan <bulgivan@mail.ru>
2021-09-23 15:10:36 +0300

    DishDetailsFragment - display data

--- app/src/main/java/com/firstproject/favdish/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/firstproject/favdish/view/fragments/DishDetailsFragment.kt
@@ -6,7 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.firstproject.favdish.R
 import com.firstproject.favdish.databinding.FragmentDishDetailsBinding
+import java.io.IOException
+import java.util.*
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -39,7 +43,32 @@ class DishDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val args: DishDetailsFragmentArgs by navArgs()
-        binding.tvTitle.text = args.dishDetails.title
+        args.let {
+            try {
+                // Load the dish image in the ImageView.
+                Glide.with(requireActivity())
+                    .load(it.dishDetails.image)
+                    .centerCrop()
+                    .into(binding.ivDishImage)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            binding.tvTitle.text = it.dishDetails.title
+            binding.tvType.text =
+                it.dishDetails.type.replaceFirstChar { char ->
+                    if (char.isLowerCase()) char.titlecase(Locale.ROOT) else char.toString() }
+            binding.tvCategory.text = it.dishDetails.category
+            binding.tvIngredients.text = it.dishDetails.ingredients
+            binding.tvCookingDirection.text = it.dishDetails.directionToCook
+            binding.tvCookingTime.text =
+                resources.getString(R.string.lbl_estimate_cooking_time, it.dishDetails.cookingTime)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 //    companion object {
 //        @JvmStatic
