7d1e92b15cef38300d3e723d079ec430c2c491c6
BulgakovIvan <bulgivan@mail.ru>
2021-10-01 13:55:39 +0300

    RandomDishViewModel, RandomDishFragment, load data form API

--- app/src/main/java/com/firstproject/favdish/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/com/firstproject/favdish/view/fragments/RandomDishFragment.kt
@@ -1,14 +1,19 @@
 package com.firstproject.favdish.view.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.firstproject.favdish.databinding.FragmentRandomDishBinding
 import com.firstproject.favdish.view.activities.MainActivity
+import com.firstproject.favdish.viewmodels.RandomDishViewModel
 
 class RandomDishFragment : Fragment() {
+
+    private val randomDishViewModel: RandomDishViewModel by viewModels()
 
     private var _binding: FragmentRandomDishBinding? = null
     private val binding get() = _binding!!
@@ -22,6 +27,13 @@ class RandomDishFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        randomDishViewModelObserver()
+        randomDishViewModel.getRandomRecipeFromAPI()
+    }
+
     override fun onResume() {
         super.onResume()
         if (requireActivity() is MainActivity) {
@@ -32,5 +44,21 @@ class RandomDishFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun randomDishViewModelObserver() {
+        randomDishViewModel.randomDishResponse.observe(viewLifecycleOwner){ response ->
+            response.let {
+                Log.e("ups", it.recipes[0].title)
+            }
+        }
+
+        randomDishViewModel.randomDishLoadingError.observe(viewLifecycleOwner) {
+            Log.e("ups", "Random dish error: $it")
+        }
+
+        randomDishViewModel.loadRandomDish.observe(viewLifecycleOwner){
+            Log.e("ups", "Random dish loading: $it")
+        }
     }
 }
--- app/src/main/java/com/firstproject/favdish/viewmodels/RandomDishViewModel.kt
+++ app/src/main/java/com/firstproject/favdish/viewmodels/RandomDishViewModel.kt
@@ -1,14 +1,46 @@
 package com.firstproject.favdish.viewmodels
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.firstproject.favdish.model.entities.RandomDish
+import com.firstproject.favdish.model.network.RandomDishApiService
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.observers.DisposableSingleObserver
+import io.reactivex.rxjava3.schedulers.Schedulers
 
-// TODO: 01.10.2021 delete
 class RandomDishViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    private val randomRecipeApiService = RandomDishApiService()
+
+    private val compositeDisposable = CompositeDisposable()
+
+    val loadRandomDish = MutableLiveData<Boolean>()
+    val randomDishResponse = MutableLiveData<RandomDish.Recipes>()
+    val randomDishLoadingError = MutableLiveData<Boolean>()
+
+    fun getRandomRecipeFromAPI() {
+        loadRandomDish.value = true
+
+        compositeDisposable.add(
+            randomRecipeApiService.getRandomDish()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<RandomDish.Recipes>(){
+                    override fun onSuccess(value: RandomDish.Recipes) {
+                        loadRandomDish.value = false
+                        randomDishResponse.value = value
+                        randomDishLoadingError.value = false
+                    }
+
+                    override fun onError(e: Throwable) {
+                        loadRandomDish.value = false
+                        randomDishLoadingError.value = true
+                        e.printStackTrace()
+                    }
+
+                })
+        )
     }
-    val text: LiveData<String> = _text
+
 }
